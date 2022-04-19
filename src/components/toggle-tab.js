9733a783889206bbcc05a4735ebb1c7307b94671
Pedro Mattos <phmattos04@gmail.com>
2022-04-19 20:24:22 -0300

    tabs slider changes

--- src/components/toggle-tab.js
+++ src/components/toggle-tab.js
@@ -2,6 +2,7 @@ import {useState} from 'react';
 import '../../styles/app.module.css';
 import Image from 'next/image';
 import Figma from '../img/figma-action-4.png';
+import Adobe from '../img/xd-action-1.png';
 
 
 
@@ -36,23 +37,38 @@ function Tabs(){
         transition: "ease 1.5s"
 
     }
+    const buttonGroup = {
+        border: "1px solid black",
+        width: "35%",
+        borderRadius: "5px",
+        display: "flex",
+        margin: "auto",
+        padding: "4px 0px"
+    }
+    const contentGroup = {
+        display: "flex",
+        justifyContent: "center"
+    }
 
     function toggleTab(index){
         setToggleState(index);
     }
     return(
         <div className="mydivs">
-            <div >
+            <div style={contentGroup}>
                 <div>
                     <div style={toggleState == 1 ? contentActive : content}>
-                        <Image src={Figma}  />
+                        <Image src={Figma}  width="940" height="590" />
                     </div>
                 </div>
                 <div>
-                    <p style={toggleState == 2 ? contentActive : content}>Content 2</p>
+                    <div style={toggleState == 2 ? contentActive : content}>
+                        <Image src={Adobe} width="940" height="590" />
+                    </div>
+                    
                 </div>
             </div>
-            <div>
+            <div style={buttonGroup}>
                 <button style={toggleState == 1 ? buttonActive : button} onClick={() => toggleTab(1)}>Figma{console.log(toggleState)}</button>
                 <button style={toggleState == 2 ? buttonActive : button} onClick={() => toggleTab(2)}>Adobe</button>
             </div>
